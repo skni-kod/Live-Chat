0df5a4b510c132fcf50f97ecb788696379e5dd72
Patryk Stasik <spam.patrykstasik@gmail.com>
2023-03-05 23:47:39 +0100

    drobne zmiany

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -10,13 +10,11 @@ class Profile extends Model
     use HasFactory;
 
     protected $table = 'profiles';
-
     protected $fillable = [ 'name',
                             'email',
                             'avatar',
                             'user_id'
     ];
-
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
@@ -24,7 +22,6 @@ class Profile extends Model
         $this->attributes['name'] = Config::get('profile.default_name');
         $this->attributes['avatar'] = Config::get('profile.default_avatar');
     }
-
     public function user()
     {
         return $this->belongsTo(User::class);
