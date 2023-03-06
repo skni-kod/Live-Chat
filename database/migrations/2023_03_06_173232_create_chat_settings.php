f2f1dd755c488c6ce42b44f6dde0e8669d971b6d
Bartomiej Stec <itscriptcoding@gmail.com>
2023-03-06 18:44:14 +0100

    Dodane tabele ustawien

---
+++ database/migrations/2023_03_06_173232_create_chat_settings.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chat_settings', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('team_id')->unsigned();
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+            $table->string('chat_title', 50);
+            $table->string('chat_color', 10);
+            $table->string('side', 20);
+            $table->string('status', 50);
+            $table->string('text_box', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chat_settings');
+    }
+};
