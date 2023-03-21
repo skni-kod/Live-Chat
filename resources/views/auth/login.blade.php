45c2a80e3d3a07643dbdf6da975ba4ce02316ef6
patrykstk <103423425+patrykstk@users.noreply.github.com>
2023-03-21 10:08:46 +0100

    Zmieniłem wygląd loginu i rejestracji

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
-        <div class="card card-plain">
+        <div class="card ">
             <div class="card-header pb-0 text-start">
                 <h4 class="font-weight-bolder">Sign In</h4>
                 <p class="mb-0">Enter your email and password to sign in</p>
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -27,8 +27,10 @@
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
     <!-- Nucleo Icons -->
+    <link rel="stylesheet" href="{{asset('css/livechat-skni.css')}}">
     <link href="{{asset('css/nucleo-icons.css')}}" rel="stylesheet" />
     <link href="{{asset('css/nucleo-svg.css')}}" rel="stylesheet" />
+
     <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <link href="{{asset('css/nucleo-svg.css')}}" rel="stylesheet" />
@@ -36,48 +38,11 @@
     <link id="pagestyle" href="{{asset('css/argon-dashboard.css?v=2.0.4')}}" rel="stylesheet" />
 </head>
 
-<body class="">
+<body class="" id="livechat-body-style">
 <div class="container position-sticky z-index-sticky top-0">
     <div class="row">
         <div class="col-12">
-            <!-- Navbar -->
-            <nav class="navbar navbar-expand-lg blur border-radius-lg top-0 z-index-3 shadow position-absolute mt-4 py-2 start-0 end-0 mx-4">
-                <div class="container-fluid">
-                    <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="{{ url('/') }}">
-                        LiveChat Login
-                    </a>
-                    <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon mt-2">
-                <span class="navbar-toggler-bar bar1"></span>
-                <span class="navbar-toggler-bar bar2"></span>
-                <span class="navbar-toggler-bar bar3"></span>
-              </span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navigation">
-                        <ul class="navbar-nav mx-auto">
-                            <li class="nav-item">
-                                <a class="nav-link d-flex align-items-center me-2 active" aria-current="page" href="{{ url('/') }}">
-                                    <i class="fa fa-chart-pie opacity-6 text-dark me-1"></i>
-                                    Dashboard
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link me-2" href="{{ url('/register') }}">
-                                    <i class="fas fa-user-circle opacity-6 text-dark me-1"></i>
-                                    Sign Up
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link me-2" href="{{ url('/login') }}">
-                                    <i class="fas fa-key opacity-6 text-dark me-1"></i>
-                                    Sign In
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-            <!-- End Navbar -->
+
         </div>
     </div>
 </div>
@@ -85,16 +50,8 @@
     <section>
         <div class="page-header min-vh-100">
             <div class="container">
-                <div class="row">
+                <div class="row justify-content-center">
                     @yield('content')
-                    <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
-                        <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-ill.jpg');
-          background-size: cover;">
-                            <span class="mask bg-gradient-primary opacity-6"></span>
-                            <h4 class="mt-5 text-white font-weight-bolder position-relative">"Attention is the new currency"</h4>
-                            <p class="text-white position-relative">The more effortless the writing looks, the more effort the writer actually put into the process.</p>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
